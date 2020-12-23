10e5054b487f8649bb597a2cc475580c03fb3bac
Denis Roenko <denis@roenko.ru>
2020-12-23 10:58:37 +0300

    Initial Commit

---
+++ rouring test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rouring test
+//
+//  Created by Роенко Денис on 23.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
